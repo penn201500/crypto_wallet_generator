e424ef05c73ef630032b1534493c7f0919bd713b
penn <penn201500@gmail.com>
2024-07-24 21:05:11 +0800

    feat: Save generated wallet addresseds in json file

---
+++ crypto_wallet/utils.py
@@ -0,0 +1,32 @@
+import json
+from datetime import datetime
+
+
+def get_formatted_datetime():
+    """
+    Get the current date and time formatted as yyyy-mm-dd_hh-mm-ss.
+    :return: Formatted date and time string
+    """
+    now = datetime.now()
+    return now.strftime("%Y-%m-%d_%H-%M-%S")
+
+
+def save_to_json_file(data, prefix='data'):
+    """
+    Save data to a JSON file with a timestamped filename.
+
+    :param data: Dictionary containing the data to save
+    :param prefix: Prefix for the filename
+    :raises IOError: If there is an issue with file writing
+    :raises TypeError: If the data provided is not serializable to JSON
+    """
+    try:
+        timestamp = get_formatted_datetime()
+        filename = f"{prefix}_{timestamp}.json"
+        with open(filename, 'w') as file:
+            json.dump(data, file, indent=4)
+        print(f"Data saved to {filename}")
+    except IOError as e:
+        print(f"An I/O error occurred: {e}")
+    except TypeError as e:
+        print(f"Data provided is not serializable to JSON: {e}")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import click
 from crypto_wallet import generate_wallet, get_balance, generate_mnemonic
+from crypto_wallet.utils import save_to_json_file
 
 
 @click.group()
@@ -17,7 +18,8 @@ def balance(address):
 
 @cli.command()
 @click.option('--mnemonic', required=False, help='The mnemonic to create a new wallet')
-def create_wallet(mnemonic):
+@click.option('--prefix', default='wallet', help='The prefix for the JSON filename')
+def create_wallet(mnemonic, prefix):
     """Create a new wallet from a mnemonic"""
     wallet_info = generate_wallet(mnemonic)
     if "error" in wallet_info:
@@ -26,6 +28,7 @@ def create_wallet(mnemonic):
         click.echo(f"Mnemonic: {wallet_info['mnemonic']}")
         click.echo(f"Address: {wallet_info['address']}")
         click.echo(f"Private Key: {wallet_info['private_key']}")
+        save_to_json_file(wallet_info, prefix)
 
 
 @cli.command(name='generate-mnemonic')
@@ -34,8 +37,6 @@ def create_mnemonic():
     click.echo(generate_mnemonic())
 
 
-# TODO: Generate mnemonic phrase
-# TODO: Save mnemonic, address, private key to a JSON file
 # TODO: Add tests for common used functions and files
 
 
