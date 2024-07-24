30b8a413d6eb10ff10c0c9412eae270adc94ab2c
penn <penn201500@gmail.com>
2024-07-25 07:20:02 +0800

    Use setup.py to: 1. Creating a resuable package 2. Defining Entry Points 3. Managing dependencies

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='crypto-wallet-cli',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'web3',
+        'mnemonic',
+    ],
+    entry_points={
+        'console_scripts': [
+            'cw=main:cli',
+        ],
+    },
+)
